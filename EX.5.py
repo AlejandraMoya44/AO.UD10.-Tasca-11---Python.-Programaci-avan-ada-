d39d51222a7391c932ecdefbf801432316bc51ea
Alejandra <amoya20949@iesjoanramis.org>
2025-03-05 16:38:04 +0100

    EX.5.py

---
+++ EX.5.py
@@ -0,0 +1,10 @@
+"""
+Crear una funció que donada una llista, retorni un diccionari que tingui com a clau: els valors de la llista i com a valor el seu índex dins la llista. 
+Utilitzar enumerate. Ex: (‘casa’,’cotxe’,’cadira’,’taula’) retorni {‘casa’:0, ‘cotxe’:1, ‘cadira’:2, ‘taula’:3}.
+"""
+def llista_a_diccionari(llista):
+    return {valor: index for index, valor in enumerate(llista)}
+
+llista = ['casa', 'cotxe', 'cadira', 'taula']
+resultat = llista_a_diccionari(llista)
+print(resultat)
